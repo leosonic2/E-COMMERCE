275b867b641debaf640aef4fb6d276b319cbaa12
Leandro Martins Fernandes <leosonic2@gmail.com>
2018-08-01 17:01:50 -0300

    Foi desenvolvido o CRUD dos usuários

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -1,56 +1,32 @@
 <?php 
+namespace Hcode;
 
-	namespace Hcode;
-
-	class Model {
-
-		private $values = [];
-
-		public function setData($data)
+class Model {
+	private $values = [];
+	public function __call($name, $args)
+	{
+		$method = substr($name, 0, 3);
+		$fieldName = substr($name, 3, strlen($name));
+		switch ($method)
 		{
-
-			foreach ($data as $key => $value)
-			{
-
-				$this->{"set".$key}($value);
-
-			}
-
+			case "get":
+				return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
+			break;
+			case "set":
+				$this->values[$fieldName] = $args[0];
+			break;
 		}
-
-		public function __call($name, $args)
-		{
-
-			$method = substr($name, 0, 3);
-			$fieldName = substr($name, 3, strlen($name));
-
-			if (in_array($fieldName, $this->fields))
-			{
-				
-				switch ($method)
-				{
-
-					case "get":
-						return $this->values[$fieldName];
-					break;
-
-					case "set":
-						$this->values[$fieldName] = $args[0];
-					break;
-
-				}
-
-			}
-
-		}
-
-		public function getValues()
-		{
-
-			return $this->values;
-
-		}
-
 	}
-
+	public function setData($data = array())
+	{
+		foreach ($data as $key => $value) {
+			
+			$this->{"set".$key}($value);
+		}
+	}
+	public function getValues()
+	{
+		return $this->values;
+	}
+}
  ?>
